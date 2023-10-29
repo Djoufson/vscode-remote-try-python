a4f66acb20b4314063f8f1a0d80212beb9c94e64
Djoufson CHE BENE <101910784+Djoufson@users.noreply.github.com>
2023-10-29 14:42:10 +0000

    feature: Game logic
    The game was designed entirely with the help of GitHub copilot.
    Following the comments in the app.py file, the code was generated accordingly.

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+import random
 
-from flask import Flask
-app = Flask(__name__)
+while True:
+    # Prompt the user to enter a choice between 'rock' 'scissors' and 'paper', and check the input validity
+    # The input must be a number and the choice menu must be displayed as a list of propositions
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+    options = ['rock', 'scissors', 'paper']
+    print("Enter a number to choose between the following options:")
+    for i, option in enumerate(options):
+        print(f"{i+1}. {option}")
+
+    while True:
+        try:
+            choice = int(input("Enter your choice: "))
+            if choice < 1 or choice > len(options):
+                print("Invalid choice. Please enter a number between 1 and", len(options))
+            else:
+                break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+    selected_option = options[choice-1]
+    print("You selected", selected_option)
+
+    # Generate a random choice for the computer
+    computer_choice = random.choice(options)
+    print("The computer selected", computer_choice)
+
+    # Confront the two choices to determine who wins the turn
+    if selected_option == computer_choice:
+        print("It's a tie!")
+    elif selected_option == 'rock' and computer_choice == 'scissors' or \
+         selected_option == 'scissors' and computer_choice == 'paper' or \
+         selected_option == 'paper' and computer_choice == 'rock':
+        print("You win!")
+    else:
+        print("The computer wins!")
+    
+    # Ask the user to play again or exit the application
+    play_again = input("Do you want to play again? (y/n): ")
+    if play_again.lower() != 'y':
+        break
+
+print("Thanks for playing!")
